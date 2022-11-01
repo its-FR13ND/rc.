7e302d7648cb3194c6013f7ea6bfee4e8cfb8685
FR13ND <FriendWith13@gmail.com>
2022-11-01 17:21:51 +0330

    initial

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,55 @@ const HomePage = () => {
                 </ul>
             </header>
             <Navigation />
-            <main></main>
+            <main className="mx-auto flex w-full max-w-lg flex-col items-start justify-start lg:max-w-5xl ">
+                <section className="flex w-full flex-col items-center justify-between gap-y-8 gap-x-10 border-b-2 border-dotted  border-orange-500 px-5 py-10 lg:flex-row lg:gap-y-0 lg:px-0">
+                    <header className="order-2 flex w-full basis-3/5 flex-col items-center justify-center gap-y-3 lg:order-1">
+                        <h2 className="bg-half-orange text-3xl font-thin text-orange-500 md:text-4xl lg:text-5xl">
+                            روغنــــــــــــــــــــــــــــــی‌کار
+                        </h2>
+                        <h1 className="text-sm font-extrabold text-stone-500 md:text-lg lg:text-2xl ">
+                            فروش انواع روغن و فیلتر خودرو به سراسر کشور
+                        </h1>
+                    </header>
+                    <figure className="relative order-1 h-64 w-full lg:order-2 lg:basis-2/5">
+                        <Image
+                            src="/static/images/OilBanner.webp"
+                            alt="روغنی کار"
+                            fill
+                            className="object-contain"
+                            quality={50}
+                            priority
+                            sizes="(max-width: 768px) 100vw"
+                        />
+                    </figure>
+                </section>
+                <section className="grid w-full grid-cols-1 items-center justify-items-center gap-6 border-b-2 border-dotted border-orange-500 py-10 lg:grid-cols-[auto_auto]">
+                    <figure className="relative row-span-2 h-[12.5rem] w-[22.75rem] overflow-hidden rounded-2xl border border-stone-300 lg:h-[20.625rem] lg:w-[37.5rem]">
+                        <Image
+                            src="/static/images/Banner1.jpg"
+                            alt="Banner1"
+                            fill
+                            className="object-cover"
+                        />
+                    </figure>
+                    <figure className="relative h-[4.875rem] w-[22.75rem] overflow-hidden rounded-2xl border border-stone-300 lg:h-[5.375rem] lg:w-[25rem]">
+                        <Image
+                            src="/static/images/Banner1.jpg"
+                            alt="Banner2"
+                            fill
+                            className="object-cover"
+                        />
+                    </figure>
+                    <figure className="relative h-[12.5rem] w-[22.75rem] overflow-hidden rounded-2xl border border-stone-300 lg:h-[13.75rem] lg:w-[25rem]">
+                        <Image
+                            src="/static/images/Banner2.jpg"
+                            alt="Banner3"
+                            fill
+                            className="object-cover"
+                        />
+                    </figure>
+                </section>
+            </main>
             <Footer />
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
             boxShadow: {
                 orange: "0px 7px 15px rgba(255, 72, 0, 0.6)",
             },
+            backgroundImage:{
+                "half-orange": "linear-gradient(0deg, rgba(249,115,22,0.3) 0%, rgba(249,115,22,0.3) 50%, rgba(249,115,22,0) 50%, rgba(249,115,22,0) 100%)"
+            }
         },
     },
     plugins: [],
